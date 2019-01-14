cde0b0ad96272671536482b7d9eab55c1126ca8b
Danielle Wood <danielle.wood9988@gmail.com>
2019-01-14 13:57:52 -0500

    functionality

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -32,6 +32,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 // =============================================================================
